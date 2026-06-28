43bf2dede41fafd4cc5fb56ead7c1c6446687c0c
Anthony Nguyen <114271592+anbguye@users.noreply.github.com>
2026-06-28 16:35:50 -0700

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.py
@@ -0,0 +1,24 @@
+class MinStack:
+
+
+    def __init__(self):
+        self.stack = []
+        self.minStack = []
+        
+    def push(self, value: int) -> None:
+        self.stack.append(value)
+
+        if not self.minStack or self.minStack[-1] > value:
+            self.minStack.append(value)
+        else:
+            self.minStack.append(self.minStack[-1])
+
+    def pop(self) -> None:
+        self.minStack.pop()
+        self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.minStack[-1]
